0fb49560e9cdede8f11a1d9d66ac2581ba866284
Florent <f@linguenheld.fr>
2022-09-22 09:00:36 +0200

    Init branch

--- rules.mk
+++ rules.mk
@@ -1,16 +1,5 @@
-# Recommended by the doc to improve compilation :
-LTO_ENABLE      = yes
-# SPLIT_KEYBOARD = yes
-
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
-
-MOUSEKEY_ENABLE = yes
-AUTO_SHIFT_ENABLE = yes
-TAP_DANCE_ENABLE = yes
-COMBO_ENABLE = yes
-
-SEND_STRING_ENABLE = yes
-
-# UNICODE_ENABLE = yes
-# UNICODEMAP_ENABLE = yes
+LTO_ENABLE      = yes
